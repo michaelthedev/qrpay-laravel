07fe9e7b6d68109797bde81ea4abc87136a784f1
Michael A <39175160+michaelthedev@users.noreply.github.com>
2024-04-19 18:16:37 +0100

    chore: add /ping endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Init
+Route::get('/ping', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => "You see this message on your screen, get ready cause i'm going hard. Bing-Bong 😂",
+    ]);
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
