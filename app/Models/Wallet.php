3a9309d90cc057710fee1c8b512499f067690a23
Michael A <39175160+michaelthedev@users.noreply.github.com>
2024-04-22 20:56:07 +0100

    chore: balance attribute modifier

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -24,5 +25,13 @@ final class Wallet extends Model
     public function transactions(): HasMany
     {
         return $this->hasMany(Transaction::class);
+    }
+
+    protected function balance(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value / 100,
+            set: fn ($value) => $value * 100
+        );
     }
 }
