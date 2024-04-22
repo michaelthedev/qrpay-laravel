3ba70af473dd752b7466ec6566c92d27a3ade248
Michael A <39175160+michaelthedev@users.noreply.github.com>
2024-04-22 20:54:22 +0100

    chore: cast transaction details as array

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -19,6 +19,10 @@ final class Transaction extends Model
         'details',
     ];
 
+    protected $casts = [
+        'details' => 'array'
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
