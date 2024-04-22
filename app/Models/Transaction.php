c8c2bdadfc14563b2bda5fd5432e93ffd6e54b55
Michael A <39175160+michaelthedev@users.noreply.github.com>
2024-04-22 20:49:08 +0100

    chore: add `transactions` migration and model

---
+++ app/Models/Transaction.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+final class Transaction extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'wallet_id',
+        'amount',
+        'type',
+        'status',
+        'details',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function wallet(): BelongsTo
+    {
+        return $this->belongsTo(Wallet::class);
+    }
+
+    protected function amount(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value / 100,
+            set: fn ($value) => $value * 100
+        );
+    }
+}
---
+++ database/migrations/2024_04_22_193741_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('user_id');
+            $table->integer('wallet_id');
+            $table->integer('amount');
+
+            $table->string('type');
+            $table->enum('status', ['pending', 'success', 'failed'])
+                ->default('pending');
+
+            $table->json('details')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
